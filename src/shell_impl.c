227044279d510e333aab43d256cccf89b6b4fb46
koreansuperfun <lucazdy@gmail.com>
2022-02-02 19:00:41 -0800

    Finished up to input.

--- src/shell_impl.c
+++ src/shell_impl.c
@@ -1,5 +1,9 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <dc_posix/dc_stdlib.h>
+#include <dc_posix/dc_regex.h>
 #include "shell_impl.h"
-
+#include "util.h"
 
 /**
  * Set up the initial state:
@@ -15,8 +19,38 @@
  * @param arg the current struct state
  * @return READ_COMMANDS or INIT_ERROR
  */
-int init_state(const struct dc_posix_env *env, struct dc_error *err, void *arg) {
-    return 0;
+int init_state(const struct dc_posix_env *env, struct dc_error *err, void *arg){
+
+    struct state *state_arg;
+    char * path;
+    char **path_array;
+    char *ps1_env_var;
+
+    state_arg = (struct state *) arg;
+
+    state_arg->max_line_length = (size_t) sysconf(_SC_ARG_MAX);
+
+    state_arg->in_redirect_regex = dc_malloc(env, err, sizeof (regex_t));
+    state_arg->out_redirect_regex = dc_malloc(env, err, sizeof (regex_t));
+    state_arg->err_redirect_regex = dc_malloc(env, err, sizeof (regex_t));
+    dc_regcomp(env, err, state_arg->in_redirect_regex, "[ \\t\\f\\v]<.*", REG_EXTENDED);
+    dc_regcomp(env, err, state_arg->out_redirect_regex, "[ \\t\\f\\v][1^2]?>[>]?.*", REG_EXTENDED);
+    dc_regcomp(env, err, state_arg->err_redirect_regex, "[ \\t\\f\\v]2>[>]?.*", REG_EXTENDED);
+
+    path = get_path(env, err);
+    path_array = parse_path(env, err, path);
+    state_arg->path = path_array;
+
+    ps1_env_var = get_prompt(env, err);
+    state_arg->prompt = ps1_env_var;
+
+    state_arg->current_line_length = 0;
+    state_arg->current_line = NULL;
+    state_arg->command = NULL;
+    state_arg->fatal_error = false;
+
+
+    return READ_COMMANDS;
 }
 
 /**
@@ -29,7 +63,40 @@ int init_state(const struct dc_posix_env *env, struct dc_error *err, void *arg) 
  */
 int destroy_state(const struct dc_posix_env *env, struct dc_error *err,
                   void *arg) {
-    return 0;
+
+    struct state *state_arg;
+    int pos;
+
+    state_arg = (struct state *) arg;
+
+    state_arg->fatal_error = false;
+    dc_free(env, state_arg->command, sizeof(struct command));
+    state_arg->current_line_length = 0;
+    dc_free(env, state_arg->current_line, sizeof(char *));
+    state_arg->max_line_length = 0;
+    dc_free(env, state_arg->prompt, sizeof(char *));
+    dc_free(env, state_arg->err_redirect_regex, sizeof(regex_t));
+    dc_free(env, state_arg->in_redirect_regex, sizeof(regex_t));
+    dc_free(env, state_arg->out_redirect_regex, sizeof(regex_t));
+
+    state_arg->command = NULL;
+    state_arg->current_line = NULL;
+    state_arg->prompt = NULL;
+    state_arg->err_redirect_regex = NULL;
+    state_arg->in_redirect_regex = NULL;
+    state_arg->out_redirect_regex = NULL;
+
+    pos = 0;
+
+    while (state_arg->path[pos] != NULL) {
+        dc_free(env, state_arg->path[pos++], sizeof(char *));
+    }
+
+    dc_free(env, state_arg->path, sizeof(char **));
+    state_arg->path = NULL;
+
+
+    return DC_FSM_EXIT;
 }
 
 /**
@@ -42,7 +109,15 @@ int destroy_state(const struct dc_posix_env *env, struct dc_error *err,
  */
 int reset_state(const struct dc_posix_env *env, struct dc_error *err,
                 void *arg) {
-    return 0;
+    struct state *state_arg;
+
+    state_arg = (struct state *) arg;
+
+    do_reset_state(env, err, state_arg);
+
+    init_state(env, err, state_arg);
+
+    return READ_COMMANDS;
 }
 
 /**
