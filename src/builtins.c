ad1cb576bd58c289f9afd11f25e0a934189063f1
koreansuperfun <lucazdy@gmail.com>
2022-02-04 21:42:51 -0800

    Finished builtins

--- src/builtins.c
+++ src/builtins.c
@@ -1,4 +1,12 @@
+#include <dc_posix/dc_string.h>
+#include <wordexp.h>
+#include <dc_posix/dc_wordexp.h>
+#include <dc_posix/dc_unistd.h>
+#include <dc_posix/dc_stdlib.h>
+#include <dc_posix/dc_stdio.h>
+#include <dc_util/path.h>
 #include "builtins.h"
+
 
 /**
  * Change the working directory.
@@ -13,5 +21,56 @@
  */
 void builtin_cd(const struct dc_posix_env *env, struct dc_error *err,
                 struct command *command, FILE *errstream) {
+    char *path;
+    int chdir_status;
 
+    if (command->argv[1]) {
+        path = dc_strdup(env, err, command->argv[1]);
+    } else {
+        path = dc_strdup(env, err, "~/");
+    }
+
+    if (dc_strstr(env, path, "~") != NULL) {
+        dc_expand_path(env, err, &path, "~");
+    }
+
+
+    chdir_status = dc_chdir(env, err, path);
+    if (chdir_status == -1) {
+
+        char *message;
+        char *dir = dc_strdup(env, err, path);
+
+        switch (err->err_code) {
+        case EACCES: case ELOOP: case ENAMETOOLONG:
+                message = dc_strdup(env, err, err->message);
+                dir = dc_realloc(env, err, dir, strlen(dir) + strlen(message));
+                sprintf(dir, "%s: %s", dir, message);
+                dc_free(env, message, strlen(message));
+                fprintf(errstream, "%s\n", dir);
+                break;
+            case ENOENT:
+                dir = dc_realloc(env, err, dir, strlen(dir) + 16);
+                sprintf(dir, "%s: does not exist", dir);
+                fprintf(errstream, "%s\n", dir);
+
+                break;
+            case ENOTDIR:
+                dir = dc_realloc(env, err, dir, strlen(dir) + 20);
+                sprintf(dir, "%s: is not a directory", dir);
+                fprintf(errstream, "%s\n", dir);
+                break;
+        }
+
+        free(dir);
+
+
+        command->exit_code = 1;
+    } else {
+        command->exit_code = 0;
+    }
+
+
+
+    dc_free(env, path, strlen(path));
 }
